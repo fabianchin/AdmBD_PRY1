e20e0da797501448387c158b4a01a96c0adef4d4
fabianchin <85766666+fabianchin@users.noreply.github.com>
2021-11-23 18:09:37 -0600

    Create index.php

---
+++ ProyectoBD2021/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTG-8">
+    <meta http-equiv="X-UA-Comparible" content="IE-edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <?php
+        include_once "./data/conectiondb.php";
+        $con = new ConectionDB();
+        $con->conection();
+    ?>
+</body>
+</html>
