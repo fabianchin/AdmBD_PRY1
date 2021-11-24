b2d1fa8641393c90a7865ad83d8f56a91597cbc6
fabianchin <85766666+fabianchin@users.noreply.github.com>
2021-11-23 18:08:56 -0600

    Create conectiondb.php

---
+++ ProyectoBD2021/data/conectiondb.php
@@ -0,0 +1,23 @@
+a<?php
+
+class ConectionDB{
+
+    function conection(){
+        $host='localhost';
+        $dbname='BDProyectoFinal';
+        $username='sa';
+        $pass='123fabichin';
+        $port=1433;
+
+        try{
+            $conn = new PDO ("sqlsrv:Server=$host,$port;Database=$dbname",$username,$pass);
+            echo "Se conecto ajuaaaa";
+        }catch(PDOException $e){
+            echo ("No se conecto a: $dbname, error: $e");
+        }
+
+    }
+
+}
+
+?>
